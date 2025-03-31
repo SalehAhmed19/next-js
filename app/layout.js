e91066d6be2f589c3d8b50d057565de494dc89a8
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-03-31 20:26:22 +0600

    making-nested-route

--- app/layout.js
+++ app/layout.js
@@ -18,9 +18,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="p-6">
+    <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-6`}
       >
         <nav>Home | About</nav>
         {children}

9a3946bda17c97a275c1f25ca3d28ff645b241d7
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-04-07 16:33:49 +0600

    dynamic metadata

---
+++ app/posts/layout.js
@@ -0,0 +1,10 @@
+export function metadata() {
+  return {
+    title: "Posts",
+    description: "All Posts",
+  };
+}
+
+export default function aboutLayout({ children }) {
+  return <div>{children}</div>;
+}
